8b6282127103f64da0df3662b8b4efae1c7ae0e2
Roshan <roshanranjith02@gmail.com>
2026-07-14 17:18:03 +0200

    Update email configuration

--- config/resend.js
+++ config/resend.js
@@ -6,7 +6,7 @@ dotenv.config();
 export const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const FROM_EMAIL =
-  'Paris Private Airport Transfer <onboarding@resend.dev>';
+  'Paris Private Airport Transfer <booking@parisprivateairporttransfer.com>';
 
 export const REPLY_TO =
   'parisprivateairporttransfer@gmail.com';
